8676c1e09cf4bb477bf714767e4f8c04b9c8ca5b
Jean-Eudes Peloye <hello@peloye.je>
2018-08-06 11:45:54 +0200

    git: add configuration file

---
+++ git/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Sets up git configuration into the user's home directory
+
+CONFIG_FILE="$HOME"/.gitconfig
+CURRENT_DIR=$(dirname "$(readlink -f "$0")")
+
+set -e
+
+echo "Set up git configuration into "$CONFIG_DIR" ..."
+if [ -f "$CONFIG_FILE" ] || [ -L "$CONFIG_FILE" ]; then
+    rm "$CONFIG_FILE"
+fi
+ln -s "$CURRENT_DIR"/config "$CONFIG_FILE"
+echo "Done."
+
