b8c3e5b9599719c395747185689a4dbc8288636e
Jean-Eudes Peloye <hello@peloye.je>
2018-08-06 11:23:20 +0200

    vim: fix install script (handling of paths)

--- vim/install.sh
+++ vim/install.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+# Sets up vim configuration into the user's home directory
+
 CONFIG_FILE="$HOME"/.vimrc
+VIM_DIR=$(dirname "$(readlink -f "$0")")
+
+set -e
 
 # Symlink vim config in the home directory
-if [ -f $CONFIG_FILE ]; then
-    rm $CONFIG_FILE
+echo "Set up vim configuration into "$HOME" ..."
+if [ -f "$CONFIG_FILE" ] || [ -L "$CONFIG_FILE" ]; then
+    rm "$CONFIG_FILE"
 fi
-ln -s "$(pwd)"/vimrc $CONFIG_FILE 
+ln -s "$VIM_DIR"/vimrc "$CONFIG_FILE"
+echo "Done."
 
 # Setup Plug plugin manager (https://github.com/junegunn/vim-plug)
+echo "Set up vim plugins ..."
 mkdir -p "$HOME"/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "Done."
 
