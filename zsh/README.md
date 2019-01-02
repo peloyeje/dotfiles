1855cbfd5675341441b9a02d872ce67ddf9aaf8e
Jean-Eudes Peloye <hello@peloye.je>
2019-01-02 18:19:01 +0100

    zsh: first commit

---
+++ zsh/README.md
@@ -0,0 +1,6 @@
+## Zsh
+
+#### Installation
+
+1. Run the `install.sh` script
+2. Enjoy !
---
+++ zsh/install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+###
+# PROGRAM configuration install script
+###
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Base variables
+SOURCE_DIR=$( cd "$( dirname "$0" )" && pwd )
+
+# Config files
+SOURCE_FILES=(zshrc)
+DEST_FILES=(.zshrc)
+
+DEST_DIR="$HOME" # The destination config directory of the program
+
+# Creates the destination folder if it doesn't exists
+if [ ! -e "$DEST_DIR" ]; then
+    mkdir -p "$DEST_DIR"
+fi
+
+# Symlink SOURCE_FILES into DEST_DIR with name DEST_FILES
+for i in ${!SOURCE_FILES[@]};
+do
+    source=${SOURCE_FILES[$i]}
+    dest=${DEST_FILES[$i]}
+
+    SOURCE_PATH="$SOURCE_DIR/$source"
+
+    if [ ! -f "$SOURCE_PATH" ]; then
+        echo "$SOURCE_PATH is missing, skipping ..."
+        continue
+    fi
+
+    DEST_PATH="$DEST_DIR/$dest"
+
+    echo "Symlinking $SOURCE_PATH to $DEST_PATH ..."
+    if [ -f "$DEST_PATH" ] || [ -L "$DEST_PATH" ]; then
+         rm "$DEST_PATH"
+    fi
+
+    ln -s "$SOURCE_PATH" "$DEST_PATH"
+
+    echo "Done."
+done
