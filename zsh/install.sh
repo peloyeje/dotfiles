26d71dda740ed4761e874dc782f1f5b4c88588ff
Jean-Eudes Peloye <hello@peloye.je>
2019-03-04 19:45:40 +0100

    zsh: move bin install into script and add gnu utils

--- zsh/install.sh
+++ zsh/install.sh
@@ -16,6 +16,17 @@ DEST_FILES=(.zshrc .zshenv)
 
 DEST_DIR="$HOME" # The destination config directory of the program
 
+# Make sure zsh is installed first
+if [ ! -x "$(command -v zsh)" ]; then
+    log "[zsh]" 'Installing zsh ...' >&2
+    if [ -x "$(command -v brew)" ]; then
+        "$(command -v brew)" install zsh
+    else
+        log "[zsh]" "brew is not installed; aborting"
+        exit 1
+    fi
+fi
+
 # Creates the destination folder if it doesn't exists
 if [ ! -e "$DEST_DIR" ]; then
     mkdir -p "$DEST_DIR"
@@ -30,18 +41,33 @@ do
     SOURCE_PATH="$SOURCE_DIR/$source"
 
     if [ ! -f "$SOURCE_PATH" ]; then
-        echo "$SOURCE_PATH is missing, skipping ..."
+        log "[zsh]" "$SOURCE_PATH is missing, skipping ..."
         continue
     fi
 
     DEST_PATH="$DEST_DIR/$dest"
 
-    echo "Symlinking $SOURCE_PATH to $DEST_PATH ..."
+    log "[zsh]" "Symlinking $SOURCE_PATH to $DEST_PATH ..."
     if [ -f "$DEST_PATH" ] || [ -L "$DEST_PATH" ]; then
          rm "$DEST_PATH"
     fi
 
     ln -s "$SOURCE_PATH" "$DEST_PATH"
 
-    echo "Done."
+    log "[zsh]" "Done."
+done
+
+# Install GNU utils and alter PATH to use them by default
+UTILS=("coreutils" "findutils" "diffutils" "gnu-sed" "gnu-tar" "grep")
+
+for util in  ${UTILS[@]}
+do
+    log "[zsh]" "Installing $util ..."
+    # Install
+    "$(command -v brew)" install $util
+    # Setup PATH to use these binaries by default
+    grep -v "$util" "$HOME/.zshrc" > "$HOME/.zshrc.tmp"
+    cat "$HOME/.zshrc.tmp" > "$HOME/.zshrc"
+    rm "$HOME/.zshrc.tmp"
+    echo -e "\n# $util\nexport PATH="/usr/local/opt/$util/libexec/gnubin:\$PATH"" >> "$HOME/.zshrc"
 done
